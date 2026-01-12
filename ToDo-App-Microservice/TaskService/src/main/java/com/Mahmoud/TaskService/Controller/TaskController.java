109ddb4a86e2551fc5d55bbe984d6ce8ac7f04cd
mahmoudashraf043 <mahmoudashraf043@gmail.com>
2026-01-12 16:01:46 +0200

    final

--- ToDo-App-Microservice/TaskService/src/main/java/com/Mahmoud/TaskService/Controller/TaskController.java
+++ ToDo-App-Microservice/TaskService/src/main/java/com/Mahmoud/TaskService/Controller/TaskController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/task")
 public class TaskController {
 
     @Autowired
